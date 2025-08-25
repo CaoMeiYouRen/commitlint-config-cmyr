fa6b0f14926c97b9f66c0fe86963e35ede554542
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-25 17:46:50 +0800

    fix: 优化描述文本，去除多余换行符

--- src/index.ts
+++ src/index.ts
@@ -41,14 +41,12 @@ export default {
                         emoji: '📚',
                     },
                     style: {
-                        description:
-                            '不影响代码含义的代码样式更改(空格、格式、缺少分号等)',
+                        description: '不影响代码含义的代码样式更改(空格、格式、缺少分号等)',
                         title: 'Styles',
                         emoji: '💎',
                     },
                     refactor: {
-                        description:
-                            '既不修复错误也不添加功能的代码更改(代码重构)',
+                        description: '既不修复错误也不添加功能的代码更改(代码重构)',
                         title: 'Code Refactoring',
                         emoji: '📦',
                     },
@@ -63,14 +61,12 @@ export default {
                         emoji: '🚨',
                     },
                     build: {
-                        description:
-                            '影响构建系统或外部依赖项的更改 (示例范围: gulp, broccoli, npm)',
+                        description: '影响构建系统或外部依赖项的更改 (示例范围: gulp, broccoli, npm)',
                         title: 'Builds',
                         emoji: '🛠',
                     },
                     ci: {
-                        description:
-                            '对我们的 CI 配置文件和脚本的更改 (示例范围: Travis, Circle, BrowserStack, SauceLabs)',
+                        description: '对我们的 CI 配置文件和脚本的更改 (示例范围: Travis, Circle, BrowserStack, SauceLabs)',
                         title: 'Continuous Integrations',
                         emoji: '⚙️',
                     },
@@ -87,35 +83,31 @@ export default {
                 },
             },
             scope: {
-                description:
-                    '此更改的范围是什么(例如组件或文件名)',
+                description: '此更改的范围是什么(例如组件或文件名): (按 Enter 键跳过)',
             },
             subject: {
-                description:
-                    '对变化写一个简短的、命令式的描述',
+                description: '对变化写一个简短的、命令式的描述:\n',
             },
             body: {
-                description: '提供更详细的更改描述',
+                description: '提供更详细的更改描述: (按 Enter 键跳过)\n',
             },
             isBreaking: {
-                description: '是否有任何重大变化(BREAKING CHANGE)?',
+                description: '是否有任何破坏性变化(BREAKING CHANGE)?',
             },
             breakingBody: {
-                description:
-                    '一个 BREAKING CHANGE 提交需要一个 body。请输入对提交本身的更长描述',
+                description: '一个 BREAKING CHANGE 提交需要一个 body。请输入对提交本身的更长描述:\n',
             },
             breaking: {
-                description: '描述重大变化',
+                description: '描述破坏性变化:\n',
             },
             isIssueAffected: {
                 description: '此更改会影响任何未解决的 issues 吗?',
             },
             issuesBody: {
-                description:
-                    '如果 issues 已关闭，则提交需要一个主体。请输入对提交本身的更长描述',
+                description: '如果 issues 已关闭，则提交需要一个 body。请输入对提交本身的更长描述:\n',
             },
             issues: {
-                description: '添加问题参考 (例如 "fix #123", "re #123".)',
+                description: '添加问题参考 (例如 "fix #123", "re #123".):\n',
             },
         },
     },
